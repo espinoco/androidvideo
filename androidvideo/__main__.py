8c1065c8fcca0b63752269e674b35566aa817902
Carlo Rodríguez <carloeduardorodriguez@gmail.com>
2016-02-12 13:06:54 -0600

    Add package structure

---
+++ androidvideo/__main__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from androidvideo import AndroidVideo
+
+
+def main(args=None):
+    """The main routine."""
+    AndroidVideo()
+
+if __name__ == "__main__":
+    main()
